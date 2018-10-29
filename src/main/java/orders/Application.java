541012ce4a39e5fc82b773c2ac0b12f0c2a32cfc
dshimo <dshi@utexas.edu>
2018-10-29 15:28:28 -0500

    dependency version, trying enableJpaRepository and taking off @Repository

--- src/main/java/orders/Application.java
+++ src/main/java/orders/Application.java
@@ -3,9 +3,11 @@ package orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableJpaRepositories(basePackages = { "orders"})
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/orders/OrdersRepository.java
+++ src/main/java/orders/OrdersRepository.java
@@ -3,11 +3,9 @@ package orders;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import orders.model.Order;
 
-@Repository
 public interface OrdersRepository extends CrudRepository<Order, String> {
 	List<Order> findByCustomerIdOrderByDateDesc(String customerId);
 }
