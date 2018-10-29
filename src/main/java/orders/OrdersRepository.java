490e83e0c81ace93cedd7ee88b53f8979c581a84
dshimo <dshi@utexas.edu>
2018-10-29 15:06:08 -0500

    @Repository annotation suggestion

--- src/main/java/orders/OrdersRepository.java
+++ src/main/java/orders/OrdersRepository.java
@@ -3,9 +3,11 @@ package orders;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import orders.model.Order;
 
+@Repository
 public interface OrdersRepository extends CrudRepository<Order, String> {
 	List<Order> findByCustomerIdOrderByDateDesc(String customerId);
 }
